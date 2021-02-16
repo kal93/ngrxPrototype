a91b5b32324cc8682d367bdd7069cbf67a2136fb
kal93 <maaz2dt@gmail.com>
2021-02-16 17:24:07 +0530

    set up project skeleton and folder structure

--- apps/ngrx-sample/src/app/app.module.ts
+++ apps/ngrx-sample/src/app/app.module.ts
@@ -1,11 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { CartItemsComponent } from './dashboard/cart-items/cart-items.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SelectableItemsComponent } from './dashboard/selectable-items/selectable-items.component';
+
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent, 
+    CartItemsComponent, 
+    DashboardComponent, 
+    SelectableItemsComponent
+  ],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  projects: ['<rootDir>/apps/ngrx-sample'],
+  projects: [
+    '<rootDir>/apps/ngrx-sample',
+    '<rootDir>/libs/core-data',
+    '<rootDir>/libs/core-state',
+    '<rootDir>/libs/material',
+  ],
 };
